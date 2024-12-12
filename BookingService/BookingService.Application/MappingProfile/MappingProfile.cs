f522bae4f21a27c160dbdd7ddd6d5f8983622ed3
SofiaS <sofia.seniv13@gmail.com>
2024-12-12 14:05:15 +0200

    Refactor MappingProfile

--- BookingService/BookingService.Application/MappingProfile/MappingProfile.cs
+++ BookingService/BookingService.Application/MappingProfile/MappingProfile.cs
@@ -9,9 +9,7 @@ namespace BookingService.Application.MappingProfile
         public MappingProfile()
         {
             CreateMap<RegisterDto, User>()
-                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Username))
-                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
-                .ForMember(dest => dest.Role, opt => opt.MapFrom(src => src.Role));
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.Username));
         }
     }
 }
