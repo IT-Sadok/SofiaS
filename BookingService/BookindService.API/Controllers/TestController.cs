eba61213b66500b17b5f13196c5542a58d440062
SofiaS <sofia.seniv13@gmail.com>
2024-12-18 23:34:20 +0200

    Authorization added

---
+++ BookingService/BookindService.API/Controllers/TestController.cs
@@ -0,0 +1,33 @@
+﻿using BookingService.Domain.Constants;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookingService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("admin")]
+        [Authorize(Roles = Roles.Admin)]
+        public IActionResult GetAdminData()
+        {
+            return Ok("This is a test endpoint for Admin role");
+        }
+
+        [HttpGet("host")]
+        [Authorize(Roles = Roles.Host)]
+        public IActionResult GetHostData()
+        {
+            return Ok("This is a test endpoint for Host role");
+        }
+
+        [HttpGet("user")]
+        [Authorize(Roles = Roles.User)]
+        public IActionResult GetUserData()
+        {
+            return Ok("This is a test endpoint for User role");
+        }
+
+    }
+}
